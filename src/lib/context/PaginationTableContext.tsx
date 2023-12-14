ca42c9db654bd7356a0711f1de166718b29bf11b
czkoudy <czkoudy@gmail.com>
2023-12-14 12:48:42 +0100

    fix: refactored some code, added length change menu

--- src/lib/context/PaginationTableContext.tsx
+++ src/lib/context/PaginationTableContext.tsx
@@ -68,7 +68,7 @@ export const PaginationTableProvider: React.FC<PaginationTableInterface> = ({
             searchData,
             [
               (item) => {
-                // if (Array.isArray(order.column)) {ddddxwxxwwx
+                // if (Array.isArray(order.column)) {
                 //   if (_.get(item, order.column[0])) {
                 //     return _.get(item, order.column)?.toLowerCase();
                 //   }
